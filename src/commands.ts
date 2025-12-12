dddc5b17800fc44f17d911c2c8ff57c016697a0f
Sacha TOPALOFF <sacha.topaloff@epitech.eu>
2025-12-12 20:08:29 +0100

    feat: create post command

--- src/commands.ts
+++ src/commands.ts
@@ -1,4 +1,4 @@
-import { App, normalizePath, Notice } from "obsidian";
+import { App, normalizePath, Notice, TFile, moment } from "obsidian";
 import JekyllComposePlugin from "./main";
 import { UserInputModal } from "./modals";
 import * as changeCase from "change-case";
@@ -16,7 +16,7 @@ export class JekyllComposeCommands {
                     "Enter title",
                     "Title cannot be empty",
                     (title: string) => {
-                        this.createJekyllFile(title, plugin.settings.draftsFolder).then(() => {
+                        this.createJekyllDraft(title, plugin.settings.draftsFolder).then(() => {
                             new Notice(`Draft "${title}" created`);
                         }).catch((error) => {
                             new Notice(`Error creating draft: ${error.message}`);
@@ -25,9 +25,38 @@ export class JekyllComposeCommands {
                 ).open();
             },
         });
+
+        plugin.addCommand({
+            id: "create-jekyll-post",
+            name: "Create post",
+            callback: () => {
+                new UserInputModal(
+                    plugin.app,
+                    "Enter title",
+                    "Title cannot be empty",
+                    (title: string) => {
+                        this.createJekyllPost(title, plugin.settings.postsFolder).then(() => {
+                            new Notice(`Post "${title}" created`);
+                        }).catch((error) => {
+                            new Notice(`Error creating post: ${error.message}`);
+                        });
+                    }
+                ).open();
+            },
+        });
     }
 
-    private async createJekyllFile(title: string, folder: string): Promise<void> {
+    private async createJekyllDraft(title: string, folder: string): Promise<void> {
+        const file = await this.createJekyllFile(title, folder);
+    }
+
+    private async createJekyllPost(title: string, folder: string): Promise<void> {
+        const file = await this.createJekyllFile(title, folder);
+        file.basename = moment().format("YYYY-MM-DD") + `-${file.name}`;
+        this.app.fileManager.renameFile(file, `${folder}/${file.basename}`);
+    }
+
+    private async createJekyllFile(title: string, folder: string): Promise<TFile> {
         const formattedTitle = changeCase.kebabCase(title.trim());
         const folderPath = normalizePath(folder);
         const filepath = normalizePath(`${folderPath}/${formattedTitle}.md`);
@@ -40,6 +69,7 @@ export class JekyllComposeCommands {
         // For later render template of the file for the front matter
         const file = await this.app.vault.create(filepath, "");
         await this.app.workspace.getLeaf(false).openFile(file);
+        return file;
     }
 
     private async ensureFolderExists(path: string) {
