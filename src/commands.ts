5639eeb1c470fbaf298a5e448011dbe9d0e135d5
Sacha TOPALOFF <sacha.topaloff@epitech.eu>
2025-12-14 13:18:15 +0100

    fix: prpoerly check if post exists before creating

--- src/commands.ts
+++ src/commands.ts
@@ -17,11 +17,10 @@ export class JekyllComposeCommands {
                     "Enter title",
                     "Title cannot be empty",
                     (title: string) => {
-                        this.createJekyllDraft(title, plugin.settings.draftsFolder).then(() => {
-                            new Notice(`Draft "${title}" created`);
-                        }).catch((error) => {
-                            new Notice(`Error creating draft: ${error.message}`);
-                        });
+                        this.createJekyllDraft(
+                            title,
+                            plugin.settings.draftsFolder
+                        );
                     }
                 ).open();
             },
@@ -36,11 +35,10 @@ export class JekyllComposeCommands {
                     "Enter title",
                     "Title cannot be empty",
                     (title: string) => {
-                        this.createJekyllPost(title, plugin.settings.postsFolder).then(() => {
-                            new Notice(`Post "${title}" created`);
-                        }).catch((error) => {
-                            new Notice(`Error creating post: ${error.message}`);
-                        });
+                        this.createJekyllPost(
+                            title,
+                            plugin.settings.postsFolder
+                        );
                     }
                 ).open();
             },
@@ -54,11 +52,20 @@ export class JekyllComposeCommands {
                     plugin.app,
                     plugin.settings.draftsFolder,
                     (file: TFile) => {
-                        this.publishJekyllDraft(file, plugin.settings.postsFolder).then(() => {
-                            new Notice(`Draft "${file.basename}" has been published`);
-                        }).catch((error) => {
-                            new Notice(`Error publishing draft: ${error.message}`);
-                        });
+                        this.publishJekyllDraft(
+                            file,
+                            plugin.settings.postsFolder
+                        )
+                            .then(() => {
+                                new Notice(
+                                    `Draft "${file.basename}" has been published`
+                                );
+                            })
+                            .catch((error) => {
+                                new Notice(
+                                    `Error publishing draft: ${error.message}`
+                                );
+                            });
                     }
                 ).open();
             },
@@ -72,11 +79,20 @@ export class JekyllComposeCommands {
                     plugin.app,
                     plugin.settings.postsFolder,
                     (file: TFile) => {
-                        this.unpublishJekyllPost(file, plugin.settings.draftsFolder).then(() => {
-                            new Notice(`Post "${file.basename}" has been unpublished`);
-                        }).catch((error) => {
-                            new Notice(`Error unpublishing post: ${error.message}`);
-                        });
+                        this.unpublishJekyllPost(
+                            file,
+                            plugin.settings.draftsFolder
+                        )
+                            .then(() => {
+                                new Notice(
+                                    `Post "${file.basename}" has been unpublished`
+                                );
+                            })
+                            .catch((error) => {
+                                new Notice(
+                                    `Error unpublishing post: ${error.message}`
+                                );
+                            });
                     }
                 ).open();
             },
@@ -87,16 +103,26 @@ export class JekyllComposeCommands {
         title: string,
         folder: string
     ): Promise<void> {
-        const file = await this.createJekyllFile(title, folder);
+        try {
+            await this.createJekyllFile(title, folder);
+            new Notice(`Draft "${title}" created`);
+        } catch (error) {
+            new Notice(`Error creating draft: ${error.message}`);
+        }
     }
 
     private async createJekyllPost(
         title: string,
         folder: string
     ): Promise<void> {
-        const file = await this.createJekyllFile(title, folder);
-        file.basename = moment().format("YYYY-MM-DD") + `-${file.name}`;
-        this.app.fileManager.renameFile(file, `${folder}/${file.basename}`);
+        try {
+            const date = moment().format("YYYY-MM-DD");
+            title = `${date}-${title.trim()}`;
+            await this.createJekyllFile(title, folder);
+            new Notice(`Post "${title}" created`);
+        } catch (error) {
+            new Notice(`Error creating post: ${error.message}`);
+        }
     }
 
     private async createJekyllFile(
@@ -147,7 +173,6 @@ export class JekyllComposeCommands {
                 `A post with the name "${postFileName}" already exists`
             );
         }
-
         await this.app.vault.rename(file, postFilePath);
     }
 
@@ -163,9 +188,7 @@ export class JekyllComposeCommands {
         const draftFilePath = normalizePath(`${folderPath}/${draftFileName}`);
 
         if (await this.app.vault.adapter.exists(draftFilePath)) {
-            throw new Error(
-                `Draft "${draftFileName}" already exists`
-            );
+            throw new Error(`Draft "${draftFileName}" already exists`);
         }
         await this.app.vault.rename(file, draftFilePath);
     }
